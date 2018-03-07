acc77ec063d6bd49ce039bba8d90a62efae645ab
GaneshSPatil <ganeshpl@thoughtworks.com>
2018-03-07 22:17:55 +0530

    Add type check util and implement asserthat-type plugin endpoint

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const util = require('./util');
+
+module.exports = (assert) => {
+  assert.addExtension('is');
+  assert.forExtension('is').addExtension('a');
+  assert.forExtension('is').addExtension('an');
+
+  assert.forExtension('is').forExtension('an')
+    .addMethod('array', (actual) => () => {
+      if (!util.isArray(actual)) {
+        assert.fail('Expected %s to be an array.', [actual]);
+      }
+    });
+
+  assert.forExtension('is').forExtension('a')
+    .addMethod('number', (actual) => () => {
+      if (!util.isNumber(actual)) {
+        assert.fail('Expected %s to be a number.', [actual]);
+      }
+    })
+    .addMethod('string', (actual) => () => {
+      if (!util.isString(actual)) {
+        assert.fail('Expected %s to be a string.', [actual]);
+      }
+    });
+};
---
+++ src/util.js
@@ -0,0 +1,5 @@
+module.exports = {
+  isNumber: (value) => typeof value === 'number',
+  isArray:  (value) => typeof value === 'string',
+  isString: (value) => typeof value === 'array'
+};
