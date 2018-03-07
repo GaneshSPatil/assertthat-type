e2181a3562b8994305c6b3ccddba1a6dbbf83bf9
GaneshSPatil <ganeshpl@thoughtworks.com>
2018-03-07 22:26:09 +0530

    Introduce eslint

--- src/index.js
+++ src/index.js
@@ -5,20 +5,20 @@ module.exports = (assert) => {
   assert.forExtension('is').addExtension('a');
   assert.forExtension('is').addExtension('an');
 
-  assert.forExtension('is').forExtension('an')
-    .addMethod('array', (actual) => () => {
+  assert.forExtension('is').forExtension('an').
+    addMethod('array', (actual) => () => {
       if (!util.isArray(actual)) {
         assert.fail('Expected %s to be an array.', [actual]);
       }
     });
 
-  assert.forExtension('is').forExtension('a')
-    .addMethod('number', (actual) => () => {
+  assert.forExtension('is').forExtension('a').
+    addMethod('number', (actual) => () => {
       if (!util.isNumber(actual)) {
         assert.fail('Expected %s to be a number.', [actual]);
       }
-    })
-    .addMethod('string', (actual) => () => {
+    }).
+    addMethod('string', (actual) => () => {
       if (!util.isString(actual)) {
         assert.fail('Expected %s to be a string.', [actual]);
       }
--- src/util.js
+++ src/util.js
@@ -1,5 +1,5 @@
 module.exports = {
-  isNumber: (value) => typeof value === 'number',
-  isArray:  (value) => typeof value === 'string',
-  isString: (value) => typeof value === 'array'
+  'isArray': (value) => typeof value === 'string',
+  'isNumber': (value) => typeof value === 'number',
+  'isString': (value) => Array.isArray(value)
 };
